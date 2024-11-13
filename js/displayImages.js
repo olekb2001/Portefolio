ab47b7fb97a996325f3491a1942c8f2221460385
olekb2001 <olekb2001@gmail.com>
2024-11-13 13:39:44 +0100

    img fix

---
+++ js/displayImages.js
@@ -0,0 +1,55 @@
+const imageArray = [
+{
+    id: 1,
+    title: 'Image 1',
+    img: 'https://picsum.photos/500/500',
+    alt: "Image 1"
+},
+{
+    id: 2,
+    title: 'Image 2',
+    img: '/imgTaken/the mask bilde ferdig-kopi.jpg',
+    alt: "Image 2"
+},
+{
+    id: 3,
+    title: 'Image 3',
+    img: 'https://picsum.photos/500/500',
+    alt: "Image 3"
+},
+{
+    id: 4,
+    title: 'Image 4',
+    img: 'https://picsum.photos/500/500',
+    alt: "Image 4"
+},
+{
+    id: 5,
+    title: 'Image 5',
+    img: 'https://picsum.photos/500/500',
+    alt: "Image 5"
+}
+];
+
+const imageContainer = document.querySelector('.images-display');
+
+imageArray.forEach((image) => {
+    // create div for image
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('image-item');
+    // create image element'
+    const imgEl = document.createElement('img');
+    imgEl.src = image.img;
+    imgEl.alt = image.alt;
+    // create title element
+    const titleEl = document.createElement('h3');
+    titleEl.textContent = image.title;
+
+    // append image and title to imageDiv
+    imageDiv.appendChild(titleEl);
+    imageDiv.appendChild(imgEl);
+    
+
+    // append imageDiv to imageContainer
+    imageContainer.appendChild(imageDiv);
+});
